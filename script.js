6bd846f5512700878b14d44792dd8455555a47dd
georgettenpm <georgette949@hotmail.es>
2023-01-08 21:58:48 +0100

    primer cambio

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+      var botonElement = document.getElementById("botonClick");
+      var pElement = document.getElementById("areaContador");
+      var contador = 0;
+      botonElement.onclick = function () {
+          contador++;
+          pElement.textContent = contador;
+      }
+
