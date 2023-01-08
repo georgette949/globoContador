c7d0f0e73dc8c886a18e08b5a03888a3b49e09d3
georgettenpm <georgette949@hotmail.es>
2023-01-08 22:52:14 +0100

    segundo cambio

--- script.js
+++ script.js
@@ -6,5 +6,15 @@
       botonElement.onclick = function () {
           contador++;
           pElement.textContent = contador;
+          var balloon = document.getElementById("globo");
+         balloon.style.width = "300px";
+         balloon.style.height = "300px";
+
       }
 
+
+/*document.getElementById("botonClick").onclick = function(){
+    var balloon = document.getElementById("globo");
+    balloon.style.width = "300px";
+    balloon.style.height = "300px";
+}*/
